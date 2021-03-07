bd754e7e9088d5a287c6204db56663dacf292029
Projectmaster122 <68382320+Projectmaster122@users.noreply.github.com>
2021-03-07 19:33:15 +0200

    Add files via upload

---
+++ Program.cs
@@ -0,0 +1,31 @@
+﻿using System;
+
+namespace PMOS
+{
+    class os
+    {
+        public static void echos(string input)
+        {
+            Console.WriteLine(input);
+        }
+    }
+    class Program
+    {
+        static void krnl()
+        {
+            Console.WriteLine("Welcome to the PMOS kernel!\nPlease use our commands to control the os");
+            bool isActivated = false;
+            string cmd = Console.ReadLine();
+            if (cmd == "echos")
+            {
+                string  e = Console.ReadLine();
+                os.echos(e);
+                krnl();
+            }
+        }
+        static void Main(string[] args)
+        {
+            Program.krnl();
+        }
+    }
+}
